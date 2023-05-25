fd236db75ce7ae8abc6847891acb4d7b58507165
Anton Talashmanov <talashmanovhm@gmail.com>
2023-05-25 23:06:27 +0500

    fix: make mapping implicit sinse 'ForAllOtherMembers' is not supported anymore

--- Application/Organization/Mapper/OrganizationMappingProfile.cs
+++ Application/Organization/Mapper/OrganizationMappingProfile.cs
@@ -8,13 +8,8 @@ public class OrganizationMappingProfile : Profile
 {
 	public OrganizationMappingProfile()
 	{
-		CreateMap<OrganizationModel, OrganizationDto>(MemberList.Destination)
-			.ForMember(dest => dest.Id, opt => opt.MapFrom(src => src.Id))
-			.ForMember(dest => dest.Name, opt => opt.MapFrom(src => src.Name))
-			.ForAllMembers(opt => opt.Ignore());
+		CreateMap<OrganizationModel, OrganizationDto>(MemberList.Destination);
 		
-		CreateMap<OrganizationCreateDto, OrganizationModel>(MemberList.Destination)
-			.ForMember(dest => dest.Name, opt => opt.MapFrom(src => src.OrganizationName))
-			.ForAllMembers(opt => opt.Ignore());
+		CreateMap<OrganizationCreateDto, OrganizationModel>(MemberList.Source);
 	}
 }
